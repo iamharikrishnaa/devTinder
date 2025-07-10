b1153df8f880590c7755e88ba26a633992c3bbad
iamharikrishnaa <iamharikrishnaa@gmail.com>
2025-07-10 17:11:06 +0530

    handling error

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ const express = require('express')
 
 const app =  express()
 
-const {adminAuth} = require('./middlewares/auth')
-
-app.use("/admin",adminAuth)
-
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("all data send")
+app.get("/getUserData",(req,res)=>{
+    throw new Error('gfghfhg')
+    res.send('user data sent')
 })
+
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send('something went wrong');
+    }
+})
+
 app.listen(7777,()=>{
     console.log('server is listening on port 7777')
 })
