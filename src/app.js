ba97cfae3c8109598e036e600357b15918fef82a
iamharikrishnaa <iamharikrishnaa@gmail.com>
2025-07-10 16:53:00 +0530

    auth is created

--- src/app.js
+++ src/app.js
@@ -2,20 +2,22 @@ const express = require('express')
 
 const app =  express()
 
-app.use("/user",(req,res,next)=>{
-    //route handler
-    console.log('handling the route user 1')
-    next();
-    res.send("route handler 1")
-    
-},
-(req,res)=>{
-    //route handler
-    console.log('handling the route user 2')
-    res.send("route handler 2")}
+app.use("/admin",(req,res,next)=>{
+    const token = 'abcd'
+    const isAuthorised = token === 'abc'
+    if(!isAuthorised){
+        res.status(401).send('unauthorised')
+    }
+    else{
+        next()
+    }
 
-)
+})
 
+
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("all data send")
+})
 app.listen(7777,()=>{
     console.log('server is listening on port 7777')
 })
